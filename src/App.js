93105894db3660bac165210f5a34ae9067308dca
perspicacious-one <nelsondcraig@gmail.com>
2017-12-15 22:13:37 -0800

    added firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+
+
+<script src="https://www.gstatic.com/firebasejs/4.8.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAuIYeda7Jt2QzOKoq88FT93rGqkgS-uBU",
+    authDomain: "bloc-chat-70dee.firebaseapp.com",
+    databaseURL: "https://bloc-chat-70dee.firebaseio.com",
+    projectId: "bloc-chat-70dee",
+    storageBucket: "bloc-chat-70dee.appspot.com",
+    messagingSenderId: "250506084339"
+  };
+  firebase.initializeApp(config);
+</script>
 
 class App extends Component {
   render() {
---
+++ src/Components/RoomList/RoomList.js
@@ -0,0 +1,9 @@
+import React {Component} from 'React';
+
+class RoomList extends Component {
+  constructor {
+    
+  }
+}
+
+export default RoomList;
