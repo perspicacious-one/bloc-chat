376871f951e12ceba5bb948783188e1597b06dee
perspicacious-one <nelsondcraig@gmail.com>
2017-12-18 16:25:22 -0800

    reinstalled packages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import RoomList from './Components/RoomList/RoomList.js';
 import firebase from './firebase.js';
