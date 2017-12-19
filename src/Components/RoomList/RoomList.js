46d33338b320ba97ac4d9e0714d4a8ff700b9fd0
perspicacious-one <nelsondcraig@gmail.com>
2017-12-19 12:32:40 -0800

    changes to add. Created delete

--- src/Components/RoomList/RoomList.js
+++ src/Components/RoomList/RoomList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import AddIcon from '../../assets/icons/ic_add_24px.svg';
+import AddIcon from './icons/ic_add_24px.svg';
 import './room-list.css';
 
 class RoomList extends Component {
@@ -9,11 +9,11 @@ class RoomList extends Component {
     this.roomsRef = this.props.firebase.database().ref('rooms');
 
     this.state = {
-      newRoomName: 'Create room',
+      newRoom: '',
       rooms: []
     }
-
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.createRoom = this.createRoom.bind(this);
   }
   componentDidMount() {
     this.roomsRef.on('child_added', snapshot => {
@@ -22,6 +22,7 @@ class RoomList extends Component {
       this.setState({ rooms: this.state.rooms.concat( room ) });
     });
   }
+
   handleInputChange(e) {
     const value = e.target.value;
     this.setState({
@@ -30,23 +31,36 @@ class RoomList extends Component {
   }
   createRoom() {
     this.roomsRef.push({
-      name: this.state.newRoomName
+      name: this.state.newRoom
+    });
+    this.setState({
+      newRoom: 'Create room'
     });
   }
+  deleteRoom(room, key, e) {
+    e.preventDefault();
+    const delRoom = this.props.firebase.database().ref('rooms/'+ key);
+    const i = this.state.rooms.indexOf(room);
+    delRoom.remove();
+    this.setState({
+      rooms: this.state.rooms.filter( item => item.key != key)
+    });
+  }
+
   render() {
     return (
       <ul className="room-list">
         {
           this.state.rooms.map( (room) => {
             return (
-              <li>{room.name}</li>
+              <li key={room.key}><span>{room.name}</span><i className="material-icons delete-icon" onClick={this.deleteRoom.bind(this, room, room.key)}>delete_forever</i></li>
             )
           })
         }
         <li>
-          <form onSubmit={this.createRoom()}>
-            <textarea id="new-room-input" value={this.state.newRoomName} onChange={this.handleInputChange}/>
-            <input type="submit" value={AddIcon} />
+          <form onSubmit={this.createRoom}>
+            <input type="text" id="new-room-input" value={this.state.newRoom} onChange={this.handleInputChange} placeholder="Create new room"/>
+            <input id="submit-icon" type="image" src={AddIcon}/>
           </form>
         </li>
       </ul>
